1fdf86ed53dcae2d4867d19d7b999cbff533d1c3
Alan Huang <hkycc04@gmail.com>
2020-02-09 12:24:59 +0800

    篩選判斷

--- text.py
+++ text.py
@@ -1,11 +1,11 @@
 #讀取共100萬筆紀錄
-#為何不用加strip
+#為何不用加strip，加入後讀取筆數有差異
 
 data = []
 count = 0
 with open('reviews.txt', 'r') as f:
     for line in f:
-        data.append(line.strip())
+        data.append(line)
         count += 1
         if count % 1000 == 0:
         	print(len(data))
@@ -15,3 +15,9 @@ sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
 print('平均長度是', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言小於100字')
