20bb82d7c6329f44d41947fc8a7da7f4fe6d96b9
Alan Huang <hkycc04@gmail.com>
2020-02-09 12:14:20 +0800

    加入平均長度

--- text.py
+++ text.py
@@ -11,3 +11,7 @@ with open('reviews.txt', 'r') as f:
         	print(len(data))
 print('檔案讀取完了, 總共有', len(data), '筆資料')
 
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('平均長度是', sum_len/len(data))
